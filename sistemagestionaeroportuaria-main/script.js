f14a0a57d404104e132fb9091d9a4414829d662b
joaquinmaldonado <2504461@ucc.edu.ar>
2025-06-02 23:08:39 -0300

    Subida inicial del proyecto aeroportuario

---
+++ sistemagestionaeroportuaria-main/script.js
@@ -0,0 +1,20 @@
+function validarLogin(event) {
+    event.preventDefault();
+
+    const usuario = document.getElementById("usuario").value;
+    const contrasena = document.getElementById("contrasena").value;
+
+    if (usuario === "adminaeropuerto" && contrasena === "Basededatos33") {
+        window.location.href = "panel.html";
+    } else {
+        alert("Usuario o contraseña incorrectos.");
+    }
+}
+
+function mostrarAlertaValija() {
+    alert("Persona agregada");
+}
+
+function mostrarAlertaVuelo() {
+    alert("Vuelo registrado satisfactoriamente");
+}
